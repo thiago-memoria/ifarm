74444e71fac016cbae4daeedfcd81b0f2214bb88
Thiago Memória Barroso <71512175+thiago-memoria@users.noreply.github.com>
2023-10-30 09:31:06 -0300

    criacao compra entity

---
+++ src/usuario/entities/compra.entity.ts
@@ -0,0 +1,43 @@
+import {Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from 'typeorm';
+
+@Entity()
+export class CompraEntity {
+
+    @PrimaryGeneratedColumn({
+        unsigned: true
+    })
+    id: number;
+
+    @Column({
+        length: 63
+    })
+    name: string;
+
+    @Column({
+        length: 127,
+        unique: true
+    })
+    email: string;
+
+    @Column({
+        length: 127
+    })
+    password: string;
+
+    @Column({
+        type: 'date',
+        nullable: true
+    })
+    birthAt: string;
+
+    @CreateDateColumn()
+    createAt: string;
+
+    @UpdateDateColumn()
+    updateAt: string;
+
+    @Column({
+        enum: [1, 2]
+    })
+    role: number;
+}
